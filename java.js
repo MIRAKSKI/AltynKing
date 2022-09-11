fb7388b8cb0d0a25e9b6ebf024a9ec70df0f6951
MIRAKSKI <106102863+MIRAKSKI@users.noreply.github.com>
2022-09-11 19:38:26 +0100

    javascript

---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById('img-01').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/GigaCalculator/')
+document.getElementById('img01').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/GraphDrawer')
+document.getElementById('img02').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/MegaCalculator/')
+document.getElementById('img03').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/Games')
