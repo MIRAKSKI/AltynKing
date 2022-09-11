738cd69283482fa46efaf8570f6a5e9489e3dd16
MIRAKSKI <106102863+MIRAKSKI@users.noreply.github.com>
2022-09-11 19:45:46 +0100

    java v1

--- java.js
+++ java.js
@@ -1,4 +1,12 @@
-document.getElementById('img-01').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/GigaCalculator/')
-document.getElementById('img01').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/GraphDrawer')
-document.getElementById('img02').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/MegaCalculator/')
-document.getElementById('img03').addEventListener('click', location.href='https://mirakski.github.io/AltynKing/Games')
+document.getElementById('img-01').addEventListener('click', function() {
+    location.href='https://mirakski.github.io/AltynKing/GigaCalculator/'
+});
+document.getElementById('img01').addEventListener('click', function() {
+  location.href='https://mirakski.github.io/AltynKing/GraphDrawer'
+});
+document.getElementById('img02').addEventListener('click', function() {
+  location.href='https://mirakski.github.io/AltynKing/MegaCalculator/'
+});
+document.getElementById('img03').addEventListener('click', function() {
+  location.href='https://mirakski.github.io/AltynKing/Games'
+});
