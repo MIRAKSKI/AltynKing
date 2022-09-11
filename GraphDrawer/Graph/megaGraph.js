88b36bf0fb2312ad59bfd73528b512625cdd2e4c
MIRAKSKI <106102863+MIRAKSKI@users.noreply.github.com>
2022-09-11 20:41:33 +0100

    graphupdate

--- GraphDrawer/Graph/megaGraph.js
+++ GraphDrawer/Graph/megaGraph.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('canvas');
 var canvasX = document.getElementById('canvasX');
 var CanvasX = document.getElementById('canvasX');
+var CanvasXXX = document.getElementById('CanvasX');
 var Yfactor;
 var Xfactor;
 var longitod = 15;
@@ -14,10 +15,10 @@ var X =[];
 var NY =[];
 var NX =[];
 var arrayP;
-var arrayN;
 var golballines = "black";
 var graphline = "blue";
 var backcolor = "white";
+var elie;
 //set numbers
 function numb() {
   canvas.width = window.innerWidth;
@@ -26,42 +27,48 @@ function numb() {
   canvasX.height = window.innerHeight;
   CanvasX.width = window.innerWidth;
   CanvasX.height = window.innerHeight;
+  CanvasXXX.width = window.innerWidth;
+  CanvasXXX.height = window.innerHeight;
   let OX = (CanvasX.width)/2;
   let OY = (CanvasX.height)/2;
   let ConTextX = CanvasX.getContext("2d");
+  ContrastXX = CanvasXXX.getContext("2d");
   var KY = OY;
   var KX = OX;
   var NKY = OY;
   var NKX = OX;
   var LX = (OX)/15;
   var KXP = OX+LX;
-  var KYP = OY+LX+5;
+  var KYP = OY+LX;
   var NKXP = OX-LX;
   var NKYP = OY-LX;
-  for (var i = 0; i < 70; i++) {
-    ConTextX.moveTo(KX,OY-3);
-    ConTextX.lineTo(KX,OY+3);
-    ConTextX.lineWidth = 0.02;
-    ConTextX.strokeStyle = golballines;
-    ConTextX.stroke();
+  ConTextX.rect(0,0,canvas.wifth,canvas.height)
+  ConTextX.sillStyle = backcolor
+  ConTextX.fill()
+  for (var i = 0; i < 15; i++) {
+    ContrastXX.moveTo(KX,OY-3);
+    ContrastXX.lineTo(KX,OY+3);
+    ContrastXX.lineWidth = 0.02;
+    ContrastXX.strokeStyle = golballines;
+    ContrastXX.stroke();
 
-    ConTextX.moveTo(OX-3,KY);
-    ConTextX.lineTo(OX+3,KY);
-    ConTextX.lineWidth = 0.02;
-    ConTextX.strokeStyle = golballines;
-    ConTextX.stroke();
+    ContrastXX.moveTo(OX-3,KY);
+    ContrastXX.lineTo(OX+3,KY);
+    ContrastXX.lineWidth = 0.02;
+    ContrastXX.strokeStyle = golballines;
+    ContrastXX.stroke();
 
-    ConTextX.moveTo(NKX,OY-3);
-    ConTextX.lineTo(NKX,OY+3);
-    ConTextX.lineWidth = 0.02;
-    ConTextX.strokeStyle = golballines;
-    ConTextX.stroke();
+    ContrastXX.moveTo(NKX,OY-3);
+    ContrastXX.lineTo(NKX,OY+3);
+    ContrastXX.lineWidth = 0.02;
+    ContrastXX.strokeStyle = golballines;
+    ContrastXX.stroke();
 
-    ConTextX.moveTo(OX-3,NKY);
-    ConTextX.lineTo(OX+3,NKY);
-    ConTextX.lineWidth = 0.02;
-    ConTextX.strokeStyle = golballines;
-    ConTextX.stroke();
+    ContrastXX.moveTo(OX-3,NKY);
+    ContrastXX.lineTo(OX+3,NKY);
+    ContrastXX.lineWidth = 0.02;
+    ContrastXX.strokeStyle = golballines;
+    ContrastXX.stroke();
 
     ConTextX.fillStyle =golballines
     ConTextX.fillText(text,KXP,OY+17)
@@ -105,94 +112,23 @@ function scale() {
   pixel = longitod/OX;
   Yfactor = OY*pixel;
   Xfactor = longitod;
-  functionX()
+  functionXgiga()
 }
-function functionX() {
-  var input = document.getElementById('inputX').value;
-  arrayP = input;
-  arrayN = input;
-  lenarray = arrayP.split("");
-  for (var i = 0; i < lenarray.length; i++) {
-    myarray01 = arrayP.replace("x^1/10", "Math.pow(i,1/10)");
-    myarray02 = myarray01.replace("x^1/9", "Math.pow(i,1/9)");
-    myarray03 = myarray02.replace("x^1/8", "Math.pow(i,1/8)");
-    myarray04 = myarray03.replace("x^1/7", "Math.pow(i,1/7)");
-    myarray05 = myarray04.replace("x^1/6", "Math.pow(i,1/6)");
-    myarray06 = myarray05.replace("x^1/5", "Math.pow(i,1/5)");
-    myarray07 = myarray06.replace("x^1/4", "Math.pow(i,1/4)");
-    myarray08 = myarray07.replace("x^1/3", "Math.pow(i,1/3)");
-    myarray09 = myarray08.replace("x^1/2", "Math.pow(i,1/2)");
-    myarray10 = myarray09.replace("abs(x)", "Math.abs(i)");
-    myarray11 = myarray10.replace("asin(x)", "Math.asin(i)");
-    myarray12 = myarray11.replace("acos(x)", "Math.acos(i)");
-    myarray13 = myarray12.replace("atan(x)", "Math.atan(i)");
-    myarray14 = myarray13.replace("exp(x)", "Math.exp(i)");
-    myarray15 = myarray14.replace("ln(x)", "Math.log(i)");
-    myarray16 = myarray15.replace("ln(x)", "Math.log(i)");
-    myarray17 = myarray16.replace("log(x)", "Math.log10(i)");
-    myarray18 = myarray17.replace("log(x)", "Math.log10(i)");
-    myarray19 = myarray18.replace("sin(x)", "Math.sin(i)");
-    myarray20 = myarray19.replace("cos(x)", "Math.cos(i)");
-    myarray21 = myarray20.replace("tan(x)", "Math.tan(i)");
-    myarray22 = myarray21.replace("x^x", "Math.pow((i),(i))");
-    myarray23 = myarray22.replace("x^10", "Math.pow(i,10)");
-    myarray24 = myarray23.replace("x^9", "Math.pow(i,9)");
-    myarray25 = myarray24.replace("x^8", "Math.pow(i,8)");
-    myarray26 = myarray25.replace("x^7", "Math.pow(i,7)");
-    myarray27 = myarray26.replace("x^6", "Math.pow(i,6)");
-    myarray28 = myarray27.replace("x^5", "Math.pow(i,5)");
-    myarray29 = myarray28.replace("x^4", "Math.pow(i,4)");
-    myarray30 = myarray29.replace("x^3", "Math.pow(i,3)");
-    arrayP = myarray30.replace("x^2", "Math.pow(i,2)");
-  }
-  for (var i = 0; i < lenarray.length; i++) {
-    arrayP = arrayP.replace("x", "i");
-  }
-  for (var i = 0; i < lenarray.length; i++) {
-    myarray01 = arrayN.replace("x^1/10", "Math.pow(-i,1/10)");
-    myarray02 = myarray01.replace("x^1/9", "Math.pow(-i,1/9)");
-    myarray03 = myarray02.replace("x^1/8", "Math.pow(-i,1/8)");
-    myarray04 = myarray03.replace("x^1/7", "Math.pow(-i,1/7)");
-    myarray05 = myarray04.replace("x^1/6", "Math.pow(-i,1/6)");
-    myarray06 = myarray05.replace("x^1/5", "Math.pow(-i,1/5)");
-    myarray07 = myarray06.replace("x^1/4", "Math.pow(-i,1/4)");
-    myarray08 = myarray07.replace("x^1/3", "Math.pow(-i,1/3)");
-    myarray09 = myarray08.replace("x^1/2", "Math.pow(-i,1/2)");
-    myarray10 = myarray09.replace("abs(x)", "Math.abs(-i)");
-    myarray11 = myarray10.replace("asin(x)", "Math.asin(-i)");
-    myarray12 = myarray11.replace("acos(x)", "Math.acos(-i)");
-    myarray13 = myarray12.replace("atan(x)", "Math.atan(-i)");
-    myarray14 = myarray13.replace("exp(x)", "Math.exp(-i)");
-    myarray15 = myarray14.replace("ln(x)", "Math.log(-i)");
-    myarray16 = myarray15.replace("ln(x)", "Math.log(-i)");
-    myarray17 = myarray16.replace("log(x)", "Math.log10(-i)");
-    myarray18 = myarray17.replace("log(x)", "Math.log10(-i)");
-    myarray19 = myarray18.replace("sin(x)", "Math.sin(-i)");
-    myarray20 = myarray19.replace("cos(x)", "Math.cos(-i)");
-    myarray21 = myarray20.replace("tan(x)", "Math.tan(-i)");
-    myarray22 = myarray21.replace("x^x", "Math.pow((-i),(-i))");
-    myarray22 = myarray21.replace("x^10", "Math.pow(-i,10)");
-    myarray23 = myarray22.replace("x^9", "Math.pow(-i,9)");
-    myarray24 = myarray23.replace("x^8", "Math.pow(-i,8)");
-    myarray25 = myarray24.replace("x^7", "Math.pow(-i,7)");
-    myarray26 = myarray25.replace("x^6", "Math.pow(-i,6)");
-    myarray27 = myarray26.replace("x^5", "Math.pow(-i,5)");
-    myarray28 = myarray27.replace("x^4", "Math.pow(-i,4)");
-    myarray29 = myarray28.replace("x^3", "Math.pow(-i,3)");
-    arrayN = myarray29.replace("x^2", "Math.pow(i,2)");
-  }
-  for (var i = 0; i < lenarray.length; i++) {
-    arrayN = arrayN.replace("x", "-i");
-  }
-  for (var i = 0; i < longitod; i++) {
-    Y[i] = eval(arrayP);
-    X[i] = i;
-    NY[i] = eval(arrayN);
-    NX[i] = -i
+function functionXgiga() {
+  var gigagraphXS = document.getElementById('inputX').value;
+  gigagraph = gigagraphXS.toLowerCase()
+  arrayP = math.parse(gigagraph);
+  eival = -1*longitod
+  elie = longitod*10
+  x=0
+  for (var i = 0; i < elie; i++) {
+    Y[i] = arrayP.evaluate({x:x});
+    X[i] = x;
+    NY[i] = arrayP.evaluate({x:-x});
+    NX[i] = -x
+    x=x+0.2;
     // Polinmal spicalfunction
   }
-  arrayP=0;
-  arrayN=0;
   play()
 }
 function play() {
@@ -202,32 +138,35 @@ function play() {
   var Ymodle = OY/(Yfactor);
   var Xmodle = OX/(Xfactor);
   ConText = canvasX.getContext("2d");
-  for (var i = 0; i < longitod; i++) {
+  ContrastXX = CanvasXXX.getContext("2d");
+  for (var i = 0; i < elie; i++) {
     Y[i] = (((Y[i]) * (Ymodle))*(-1))+OY;
     X[i] = ((X[i]) * (Xmodle))+OX;
     NY[i] = (((NY[i]) * (Ymodle))*(-1))+OY;
     NX[i] = ((NX[i]) * (Xmodle))+OX;
   }
-  for (var i = 0; i < longitod; i++) {
-    ConText.moveTo(0,OY);
-    ConText.lineTo(canvas.width,OY);
-    ConText.moveTo(OX,0);
-    ConText.lineTo(OX,canvas.height);
-    ConText.lineWidth = 0.01;
-    ConText.strokeStyle = golballines;
-    ConText.stroke();
-
-    contexT.moveTo(X[i],Y[i]);
-    contexT.lineTo(X[i+1],Y[i+1]);
-    contexT.moveTo(NX[i],NY[i]);
-    contexT.lineTo(NX[i+1],NY[i+1]);
-    contexT.lineWidth = 0.1;
-    contexT.strokeStyle = graphline;
-    contexT.stroke();
-    contexT.moveTo((canvas.width)*2,(canvas.height)*2);
+  for (var i = 0; i < elie; i++) {
+    ContrastXX.moveTo(0,OY);
+    ContrastXX.lineTo(canvas.width,OY);
+    ContrastXX.moveTo(OX,0);
+    ContrastXX.lineTo(OX,canvas.height);
+    ContrastXX.lineWidth = 0.01;
+    ContrastXX.strokeStyle = golballines;
+    ContrastXX.stroke();
 
   }
-  for (var i = 0; i < longitod; i++) {
+  for (var i = 0; i < elie; i++) {
+
+    ConText.moveTo(X[i],Y[i]);
+    ConText.lineTo(X[i+1],Y[i+1]);
+    ConText.moveTo(NX[i],NY[i]);
+    ConText.lineTo(NX[i+1],NY[i+1]);
+    ConText.lineWidth = 0.1;
+    ConText.strokeStyle = graphline;
+    ConText.stroke();
+
+  }
+  for (var i = 0; i < elie; i++) {
     Y[i] =0;
     X[i] =0;
     NY[i] =0;
@@ -235,31 +174,6 @@ function play() {
   }
 }
 numb()
-/*
-var canvas = document.getElementById('canvas');
-function play() {
-  contexT = canvas.getContext("2d");
-  var Y =[];
-  var X =[];
-  var Ymax = 262144;
-  var Xmax = 512;
-  var OX = (canvas.width)/2;
-  var OY = (canvas.height)/2;
-  var Ymodle = OY/(Ymax]);
-  var Xmodle = OX/(Xmax)
-  for (var i = 0; i < 512; i++) {
-    Y[i] = Math.pow(i,2);
-    X[i] = i;
-  }
-  for (var i = 0; i < 512; i++) {
-    contexT.moveTo(X[i],Y[i]);
-    contexT.lineTo(X[i+1],Y[i+1]);
-    contexT.stroke();
-  }
-  console.log("on")
-}
-play()
-*/
 document.addEventListener('wheel', function functionName() {
   var y = event.deltaY;
   altitued(y)
@@ -299,12 +213,12 @@ document.getElementById('SGC').addEventListener('click', function() {
   if (backcolor=="white") {
     golballines = "white";
     backcolor = "black";
-    document.getElementById('body').style.background = backcolor;
+    document.getElementById('graph-holder-giga').style.background = backcolor;
   }
   else if (backcolor=="black") {
     golballines = "black";
     backcolor = "white";
-    document.getElementById('body').style.background = backcolor;
+    document.getElementById('graph-holder-giga').style.background = backcolor;
   }
   numb()
 })
@@ -320,47 +234,5 @@ document.getElementById('MNu').addEventListener('click', function() {
 })
 
 document.getElementById('BTN').addEventListener('click', function() {
-  numb( )
+  numb()
 })
-//test poly
-/*
-var array = "(x^1/10)+(x^7)";
-lenarray = array.split("");
-for (var i = 0; i < lenarray.length; i++) {
-  myarray01 = array.replace("x^1/10", "Math.pow(i,1/10)");
-  myarray02 = myarray01.replace("x^1/9", "Math.pow(i,1/9)");
-  myarray03 = myarray02.replace("x^1/8", "Math.pow(i,1/8)");
-  myarray04 = myarray03.replace("x^1/7", "Math.pow(i,1/7)");
-  myarray05 = myarray04.replace("x^1/6", "Math.pow(i,1/6)");
-  myarray06 = myarray05.replace("x^1/5", "Math.pow(i,1/5)");
-  myarray07 = myarray06.replace("x^1/4", "Math.pow(i,1/4)");
-  myarray08 = myarray07.replace("x^1/3", "Math.pow(i,1/3)");
-  myarray09 = myarray08.replace("x^1/2", "Math.pow(i,1/2)");
-  myarray10 = myarray09.replace("abs(x)", "Math.abs(i)");
-  myarray11 = myarray10.replace("asin(x)", "Math.asin(i)");
-  myarray12 = myarray11.replace("acos(x)", "Math.acos(i)");
-  myarray13 = myarray12.replace("atan(x)", "Math.atan(i)");
-  myarray14 = myarray13.replace("exp(x)", "Math.exp(i)");
-  myarray15 = myarray14.replace("ln2(x)", "Math.ln2(i)");
-  myarray16 = myarray15.replace("ln10(x)", "Math.ln10(i)");
-  myarray17 = myarray16.replace("log(x)", "Math.log(i)");
-  myarray18 = myarray17.replace("log10(x)", "Math.log10(i)");
-  myarray19 = myarray18.replace("sin(x)", "Math.sin(i)");
-  myarray20 = myarray19.replace("cos(x)", "Math.cos(i)");
-  myarray21 = myarray20.replace("tan(x)", "Math.tan(i)");
-  myarray22 = myarray21.replace("x^10", "Math.pow(i,10)");
-  myarray23 = myarray22.replace("x^9", "Math.pow(i,9)");
-  myarray24 = myarray23.replace("x^8", "Math.pow(i,8)");
-  myarray25 = myarray24.replace("x^7", "Math.pow(i,7)");
-  myarray26 = myarray25.replace("x^6", "Math.pow(i,6)");
-  myarray27 = myarray26.replace("x^5", "Math.pow(i,5)");
-  myarray28 = myarray27.replace("x^4", "Math.pow(i,4)");
-  myarray29 = myarray28.replace("x^3", "Math.pow(i,3)");
-  array = myarray29.replace("x^2", "Math.pow(i,2)");
-}
-for (var i = 0; i < lenarray.length; i++) {
-  array = array.replace("x", "i");
-}
-var TestPOer = "Math.pow(i,2)";
-alert(eval(TestPOer))
-*/
